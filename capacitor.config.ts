a8022c60193986edebe04441c65a430882863d5d
Pedro21062014 <pedroberbisfreire@gmail.com>
2025-11-10 19:53:38 -0300

    fix

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.codegenstudio',
-  appName: 'codegenstudio',
-  webDir: 'dist'
-};
-
-export default config;
